f36c1b1dd3bbc35a6b41b7dc18bfff32ccd2592e
CodingInTheWestIndies <CodingInTheWestIndies@users.noreply.github.com>
2017-02-16 13:50:11 -0400

    added files

--- KrisClass.cpp
+++ KrisClass.cpp
@@ -3,34 +3,74 @@
 #include <string>
 #include <vector>
 #include "KrisClass.h"
-#include "implementation.cpp"
-
-KrisClass obj1;
-vector <KrisClass> list;
 
 
-using namespace std;
+int linearSearch(vector <KrisClass> data, string key);
+
+
 
 int main ()
 {
+KrisClass obj1;
+vector <KrisClass> list;
+string search_key;
+
+int result;
+
 	for (int listnum = 0; listnum < 4; listnum++)
 	{
 		obj1.SetName();
 		obj1.SetAge();
-		cin.ignore();
+		std::cin.ignore();
 		list.push_back(obj1);
-		cout << endl;
+		std::cout << endl;
 	}
 	system("cls");
-	cout << "LIST OF NAMES AND AGES" << endl;
+	std::cout << "LIST OF NAMES AND AGES" << endl;
 
 	for (int listnum = 0; listnum < 4; listnum++)
 	{
-		cout << list[listnum].GetName();
-		cout << ", Age: " << list[listnum].GetAge() << endl;
+		std::cout << list[listnum].GetName();
+		std::cout << ", Age: " << list[listnum].GetAge() << endl;
 		
 	}
 	system("pause");
+	system("cls");
+std::cout<<"Enter a value to search for: ";
+
+
+   std::cin>>search_key;
+   std::cin.ignore();
+    while(search_key != "#")//perform searches until sentinel entered
+    {
+        result = linearSearch(list, search_key);
+
+        std::cout<<"  '"<<search_key<<"' was ";
+
+        if (result == -1)
+          std::cout<<"not found";
+        else
+          std::cout<<"found at index "<<result;
+
+
+        std::cout<<endl<<endl<<"Enter a value to search for: ";
+        std::cin>>search_key;
+	std::cin.ignore(); 
+    }
+
+   std::cout<<endl<<"Program is now finished."<<endl<<endl;
+
 	return 0;
 }
+
+int linearSearch(vector <KrisClass> data, string key)
+{
+	for (int i = 0; i < 4; i++)
+	{
+		if(data[i].GetName() == key)
+		return i;
+	}
+	return -1; //not found
+
+};
 
